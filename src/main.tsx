5012e10d25135f36a381e6a19bb61e14d3e963a9
Son Hong Do <sondh0127@gmail.com>
2021-12-13 14:49:05 +0700

    add uno

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { registerMicroApps, start } from 'qiankun';
+import 'uno.css'
 
 registerMicroApps([
   {
     name: '@interactive/app',
     entry: 'https://dev-livestream.gviet.vn/interactive/app/',
     container: '#sub-app',
-    // loader: (loading) => render({ loading }),
     activeRule: '/interactive/app',
   },
 ]);
