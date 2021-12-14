4eac1cdc5e2730e3c3b296fd78da130f7a50e84b
Son Hong Do <sondh0127@gmail.com>
2021-12-14 15:19:06 +0700

    correct use of token

--- src/interactive-app.ts
+++ src/interactive-app.ts
@@ -2,7 +2,8 @@ import { initInteractiveState } from '@sigma-interactive/micro';
 
 export const actions = initInteractiveState(
   {
-    token: 'sondh0127-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiaWQiOiIyOWY2NWUwMC0xZWU1LTQwZjAtYWZhNS0zM2MyODE5ODI4ODMiLCJpc0FkbWluIjpmYWxzZSwiaWF0IjoxNjM3NDcyODkyfQ.HQoeTm1khKSurFhgyWnnrRSvoMS0IJ-IVsBln5nCD4A',
-    livestreamId: 'c9c2ebfb-2887-4de6-aec4-0a30aa848915'
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwdWJsaWNLZXkiOiJkZWZhdWx0LWFwcCIsImlhdCI6MTYzOTQ2ODA2MX0.UUgc5WhOCO8-rk6ujzqxSAItoTJKg41OTD7JHMyWU9c',
+    livestreamId: 'c9c2ebfb-2887-4de6-aec4-0a30aa848915',
+    tokenProp: 'x-backdoor'
   }
 );
